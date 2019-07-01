84fd8e0798726f4a193e3f9f1597e9479d356eb7
wuyeelok <wuyeelok@gmail.com>
2019-07-01 17:01:59 +0800

    Refactor: Add interface to get daily workout

--- src/com/luv2code/springdemo/BaseballCoach.java
+++ src/com/luv2code/springdemo/BaseballCoach.java
@@ -1,7 +1,8 @@
 package com.luv2code.springdemo;
 
-public class BaseballCoach {
+public class BaseballCoach implements Coach {
 
+	@Override
 	public String getDailyWorkout() {
 		return "Spend 30 minutes on batting practice";
 	}
