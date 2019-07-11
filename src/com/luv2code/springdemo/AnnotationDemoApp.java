a8e8d1211d2a1b17136eebba52cccabebab872ac
wuyeelok <wuyeelok@gmail.com>
2019-07-11 17:07:19 +0800

    Spring: Use default bean id annotation

--- src/com/luv2code/springdemo/AnnotationDemoApp.java
+++ src/com/luv2code/springdemo/AnnotationDemoApp.java
@@ -10,7 +10,7 @@ public class AnnotationDemoApp {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
 		// Get the bean from spring container
-		Coach myCoach = context.getBean("thatSillyCoach", Coach.class);
+		Coach myCoach = context.getBean("tennisCoach", Coach.class);
 
 		// Call a method on the bean
 		System.out.println(myCoach.getDailyWorkout());
