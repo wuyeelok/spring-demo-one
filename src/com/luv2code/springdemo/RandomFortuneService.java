bea89571f642b4fc293dde75182e6ec43eb17f11
wuyeelok <wuyeelok@gmail.com>
2019-07-13 10:13:59 +0800

    Refactor: Rename class

--- src/com/luv2code/springdemo/RandomFortuneService.java
+++ src/com/luv2code/springdemo/RandomFortuneService.java
@@ -3,7 +3,7 @@ package com.luv2code.springdemo;
 import org.springframework.stereotype.Component;
 
 @Component
-public class RandomService implements FortuneService {
+public class RandomFortuneService implements FortuneService {
 
 	@Override
 	public String getFortune() {
