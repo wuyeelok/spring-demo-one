bd879f6770da0a7896a3960e4e584dd1a8343d09
wuyeelok <wuyeelok@gmail.com>
2019-07-13 20:04:21 +0800

    Spring No XML: Use config class

---
+++ src/com/luv2code/springdemo/JavaConfigDemoApp.java
@@ -0,0 +1,27 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class JavaConfigDemoApp {
+
+	public static void main(String[] args) {
+
+		// Read spring config java class
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
+
+		// Get the bean from spring container
+		Coach theCoach = context.getBean("tennisCoach", Coach.class); // Default bean id
+		Coach mySwimCoach = context.getBean("Kelvin_Chan", Coach.class); // Explicit bean id
+
+		// Call a method on the bean
+		System.out.println(theCoach.getDailyWorkout());
+		System.out.println(mySwimCoach.getDailyWorkout());
+
+		// Call method to get daily fortune
+		System.out.println(theCoach.getDailyFortune());
+
+		// Close the context
+		context.close();
+	}
+
+}
