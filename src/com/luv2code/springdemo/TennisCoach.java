b6e3d6870b79f4804a29a8fd3d929623ea73a620
wuyeelok <wuyeelok@gmail.com>
2019-07-13 16:45:04 +0800

    Spring Bean: Test lifecycle using post construct

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -1,5 +1,8 @@
 package com.luv2code.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -25,6 +28,18 @@ public class TennisCoach implements Coach {
 	 * System.out.println(">> Tenniscoach: inside doSomeCrazyStuff() method");
 	 * this.fortuneService = fortuneService; }
 	 */
+
+	// Define my init method
+	@PostConstruct
+	public void doMyStartupStuff() {
+		System.out.println(">> TennisCoach: inside of doMyStartupStuff()");
+	}
+
+	// Define my destroy method
+	@PreDestroy
+	public void doMyCleanupStuff() {
+		System.out.println(">> TennisCoach: inside of doMyCleanupStuff()");
+	}
 
 	@Override
 	public String getDailyWorkout() {
