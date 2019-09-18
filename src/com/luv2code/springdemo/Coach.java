901d2762274afb5d0835a790dd90928ff550e954
wuyeelok <wuyeelok@gmail.com>
2019-09-18 09:37:50 +0800

    Remove old project files

--- src/com/luv2code/springdemo/Coach.java
+++
@@ -1,9 +0,0 @@
-package com.luv2code.springdemo;
-
-public interface Coach {
-
-	String getDailyWorkout();
-
-	String getDailyFortune();
-
-}
--- src/com/luv2code/springdemo/FortunePropService.java
+++
@@ -1,7 +0,0 @@
-package com.luv2code.springdemo;
-
-import java.util.Map;
-
-public interface FortunePropService {
-	Map<String, String> getPropMap();
-}
