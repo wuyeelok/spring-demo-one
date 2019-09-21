c9261b43010f190c2885258e46e16cdde96b7dd8
wuyeelok <wuyeelok@gmail.com>
2019-09-21 11:19:17 +0800

    Swich order of logging to see effect

--- src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -12,17 +12,17 @@ public class MyDemoLoggingAspect {
 
 	// Let's start with an @Before advice
 
-	@Before("execution(public void com.luv2code.aopdemo.dao.AccountDAO.addAccount())")
-	public void beforeAddAccountAdvice() {
-
-		System.out.println("\n====>>> Executing @Before advice on interface AccountDAO addAccount()");
-
-	}
-
 	@Before("execution(public void com.luv2code.aopdemo.dao.AccountDAOImpl.addAccount())")
 	public void beforeAddAccountImplAdvice() {
 
 		System.out.println("\n====>>> Executing @Before advice on class AccountDAOImpl addAccount()");
+
+	}
+
+	@Before("execution(public void com.luv2code.aopdemo.dao.AccountDAO.addAccount())")
+	public void beforeAddAccountAdvice() {
+
+		System.out.println("\n====>>> Executing @Before advice on interface AccountDAO addAccount()");
 
 	}
 
